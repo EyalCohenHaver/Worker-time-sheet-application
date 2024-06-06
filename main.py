f682aaffae7016e5f5a574c7712588c072250dfe
Eyal Cohen Haver <mellvinch@gmail.com>
2024-06-06 20:13:56 +0300

    udated 2 files (main.py, worker.py)

--- main.py
+++ main.py
@@ -1,32 +1,11 @@
 from worker import Worker
-from datetime import date, datetime
-import openpyxl
-import pandas as pd
-
+from time import sleep
 
 def main():
     john = Worker("John", "Walevsky", "123", 40)
-    today = date.today()
-    entryH = datetime.now().strftime("%H:%M:%S")
-
-
-
-
-    workbook = openpyxl.load_workbook(john.time_sheet_filename)
-    sheet = workbook.active
-
-
-    
-    data = [
-        ["date", "name", "worker ID", "entry time"],
-        [today, john.first_name, john.workerID, entryH],
-    ]
-
-
-
-    for row in data:
-        sheet.append(row)
-    workbook.save(john.time_sheet_filename)
+    john.clockIn()
+    sleep(10)
+    john.clockOut()
 
 
 if __name__ == "__main__":
--- worker.py
+++ worker.py
@@ -1,7 +1,9 @@
 import openpyxl
+from datetime import date, datetime, timedelta
 
 class Worker:
     def __init__(self, first_name, last_name, workerID, paygrade):
+        self.line_data = []
         self.first_name = first_name
         self.last_name = last_name
         self.workerID = workerID
@@ -11,3 +13,16 @@ class Worker:
         self.time_sheet.save(self.time_sheet_filename)
 
 
+    def clockIn(self):
+        self.line_data = [date.today(), self.first_name, self.last_name, self.workerID, datetime.now().strftime("%H:%M")]
+
+
+    def clockOut(self):
+        self.line_data.append(datetime.now().strftime("%H:%M"))
+        #TODO: fix the time delta
+        self.line_data.append(self.line_data[5] - self.line_data[4])
+        workbook = openpyxl.load_workbook(self.time_sheet_filename)
+        sheet = workbook.active
+        sheet.append(self.line_data)
+        workbook.save(self.time_sheet_filename)
+        self.line_data = []
